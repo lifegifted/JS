f1bdf28a5b40d40c609d6e2306ef2df62d9670ea
lifegifted <lifegifted@gmail.com>
2016-07-19 12:11:39 +0300

    решил задачу «Проверка стандарта»

---
+++ if-cases.js
@@ -0,0 +1,9 @@
+
+var jsStandartAnswer = prompt("Каково «официальное» название JavaScript?", '1');
+if ( jsStandartAnswer == "ECMAScript" ) {
+  alert( 'Верно!' );
+}
+else {
+  alert('Не знаете? «ECMAScript»!');
+}
+alert( jsStandartAnswer );
